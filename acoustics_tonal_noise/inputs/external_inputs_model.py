0d23211fd0ee9cf4b88cad6ccaf6b073d9e9a796
Marius Ruh <mruh@ucsd.edu>
2022-08-04 11:27:34 -0700

    commented out prin_var in external_inputs_model

--- acoustics_tonal_noise/inputs/external_inputs_model.py
+++ acoustics_tonal_noise/inputs/external_inputs_model.py
@@ -19,10 +19,11 @@ class ExternalInputsModel(Model):
         num_azimuthal = shape[2]
 
         # self.create_input('mode',shape = (frequency_mode,))
-
+        ft_to_m = 0.3048 
         omega = self.declare_variable('omega', shape=(num_nodes, 1), units='rpm')
-        self.print_var(omega)
-        thrust_origin = self.declare_variable(T_o_name_list[0], shape=(num_nodes,3))#, val=np.array([[1.829,0,3.16757]]))
+        # self.print_var(omega)
+        thrust_origin = self.declare_variable(T_o_name_list[0], shape=(num_nodes,3)) #, val=np.array([[1.829,0,3.16757]]))
+        # self.print_var(thrust_origin)       
         # self.print_var((thrust_origin[0,2]**2)**0.5)
         # thrust_vector = self.declare_variable(name='thrust_vector', shape=(num_nodes,3))
 
